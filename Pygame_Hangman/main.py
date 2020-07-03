8c330f394546774a6f2d4fdbf1229cfec987f5c2
Lakshita Jain <Lak.Jai110@gmail.com>
2020-07-03 12:46:44 +0000

    Fixed image sourcing

--- Pygame_Hangman/main.py
+++ Pygame_Hangman/main.py
@@ -10,19 +10,19 @@ pygame.display.set_caption("Hangman Game!")
 # load images
 images = []
 for i in range(7):
-    image = pygame.image.load("hangman" + str(i) + ".png")
+    image = pygame.image.load("Pygame_Hangman/hangman" + str(i) + ".png")
     images.append(image)
 
 # # button variables 
-# RADIUS = 20
-# GAP = 15
-# letters = []
-# startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
-# starty = 400
-# for i in range(26):
-#     x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
-#     y = starty + ((i // 13)) * (GAP + RADIUS * 2)
-#     letters.append([x,y])
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = starty + ((i // 13)) * (GAP + RADIUS * 2)
+    letters.append([x,y])
 
 # game variables
 hangman_status = 0
@@ -40,9 +40,9 @@ def draw():
     win.fill(WHITE)
 
     # draw buttons 
-    # for letter in letters:
-    #     x,y = letter
-    #     pygame.draw.circle(win, BLACK, (x,y), RADIUS, width=3)
+    for letter in letters:
+        x,y = letter
+        pygame.draw.circle(win, BLACK, (x,y), RADIUS, 3)
 
     win.blit(images[hangman_status], (150,100))
     pygame.display.update()
@@ -60,5 +60,5 @@ while run:
             print(pos)
 
 
-# pygame.quit()
+pygame.quit()
 
